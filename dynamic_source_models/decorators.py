83f9d57ade1aee3fe4d2985fd51567100c60529a
Roman Lee <romanlee1996@gmail.com>
2020-11-16 22:24:06 +0600

    Add source_raw decorator

--- dynamic_source_models/decorators.py
+++ dynamic_source_models/decorators.py
@@ -36,3 +36,34 @@ def source_queryset(callable=False):
         return _callable_source_queryset
     else:
         return _property_source_queryset
+
+
+def _property_source_raw(func):
+    def inner_wrapper(cls, *args, **kwargs):
+        try:
+            raw_sql, params = func(cls, *args, **kwargs)
+        except ValueError:
+            raise ValueError('Source raw method must return tuple with raw_sql and params')
+        return cls.from_raw(raw_sql, params)
+    return _classproperty(inner_wrapper)
+
+
+def _callable_source_raw(func):
+    def wrapper(*args, **kwargs):
+        def inner_wrapper(cls, *args, **kwargs):
+            try:
+                raw_sql, params = func(cls, *args, **kwargs)
+            except ValueError:
+                raise ValueError('Source raw method must return tuple with raw_sql and params')
+            return cls.from_raw(raw_sql, params)
+        return inner_wrapper(*args, **kwargs)
+    return classmethod(wrapper)
+
+
+def source_raw(callable=False):
+    if isinstance(callable, types.FunctionType):
+        return _property_source_raw(callable)
+    if callable:
+        return _callable_source_raw
+    else:
+        return _property_source_raw
